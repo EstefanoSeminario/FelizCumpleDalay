45b9ee9443a2c96a0da84a842d83f3ea9f7d8a72
Estefano Seminario <U20303303@UTP.EDU.PE>
2026-02-01 14:45:13 -0500

    musica2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-new Swiper('.swiper', {
+// Inicializar Swiper
+const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   grabCursor: true,
   resistanceRatio: 0.6,
@@ -16,4 +17,16 @@ new Swiper('.swiper', {
       opacity: 0.7,
     },
   },
+});
+
+// ---- MÚSICA DE FONDO ----
+const music = document.getElementById('bgMusic');
+let musicStarted = false;
+
+// Reproducir música al primer clic en el Swiper
+swiper.el.addEventListener('click', () => {
+  if (!musicStarted) {
+    music.play().catch(err => console.log('Autoplay bloqueado hasta interacción'));
+    musicStarted = true;
+  }
 });
